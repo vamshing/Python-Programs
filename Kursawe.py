9aa189a81c64d44423bd26816fc1aa872f99109a
Guduguntla Vamshi <vamshing@gmail.com>
2015-11-24 16:46:20 -0500

    Update Kursawe.py

--- Kursawe.py
+++ Kursawe.py
@@ -1,13 +1,13 @@
-__author__ = 'Vamshi Guduguntla'
-__copyright__ = "NA"
-__license__ = "NA"
-__version__ = "1.0"
 
 from __future__ import print_function, division
 from time import strftime
 from pprint import pprint
 import math,random,numpy as np
 
+__author__ = 'Vamshi Guduguntla'
+__copyright__ = "NA"
+__license__ = "NA"
+__version__ = "1.0"
 
 
 class Kursawe:
