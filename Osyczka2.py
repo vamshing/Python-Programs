797e4916a6300d50a34c6addf436134b7bf591c4
Guduguntla Vamshi <vamshing@gmail.com>
2015-11-24 16:46:55 -0500

    Update Osyczka2.py

--- Osyczka2.py
+++ Osyczka2.py
@@ -1,12 +1,14 @@
+from __future__ import print_function, division
+from time import strftime
+from pprint import pprint
+import math,random,numpy as np
+
 __author__ = 'Vamshi Guduguntla'
 __copyright__ = "NA"
 __license__ = "NA"
 __version__ = "1.0"
 
-from __future__ import print_function, division
-from time import strftime
-from pprint import pprint
-import math,random,numpy as np
+
 
 class Osyczka2:
     
